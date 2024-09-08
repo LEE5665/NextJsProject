6b4a717264e7d13185aacef1676c7748f25bdeb9
LEE <yttr309@naver.com>
2024-09-08 23:47:44 +0900

    init

---
+++ nextjs/app/list/page.js
@@ -0,0 +1,10 @@
+import { Lists, Listss} from './component.js'
+
+export default function list() {
+    return (
+      <div>
+        <p className="go">안녕222</p>
+        <Lists/>
+      </div>
+    );
+  }
